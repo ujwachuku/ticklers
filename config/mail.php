6f0b7ce193c3f7fb3914ed8737e77fbf908b82e4
Uzoma Jaja-Wachuku <ujwachuku@gmail.com>
2018-07-30 02:11:37 +0300

    Changed email settings in config/mail.php

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@ticklersfood.com'),
+        'name' => env('MAIL_FROM_NAME', 'Ticklers Quick Service Restaurant'),
     ],
 
     /*
