ce9c27b3a856ec99eaf9a48dc39613c9828742b2
Uzo <ujwachuku@gmail.com>
2020-03-22 22:42:11 +0200

    Added yandex verification code to header

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
       <meta name="format-detection" content="telephone=no" />
       <link rel="shortcut icon" href="/img/favicon.png"/>
+      <meta name="yandex-verification" content="5bc2db0ab51a1a3e" />
       <title>Ticklers Food - @yield('title')</title>
       <style id="dynamic-css"></style>
       <style type="text/css">
